cb3dad99c7491cfb7e949d1bb17a699e29a3a1e9
TylerFord <tford5@hawk.iit.edu>
2023-01-20 15:45:45 -0600

    Initial Commit

---
+++ Assignment 2 - Quadradic Equation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Assignment 2 - Quadradic Equation
+//
+//  Created by IIT phys 440 on 1/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
